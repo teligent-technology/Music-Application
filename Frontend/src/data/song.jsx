b6c275fffb2ce487e9275387912c0742d030f7b3
Aditya <ajha48450@gmail.com>
2025-05-09 21:33:17 +0530

    Some changes

--- Frontend/src/data/song.jsx
+++ Frontend/src/data/song.jsx
@@ -5,11 +5,11 @@ export const punjabiSongs = [
     { Id: 2,song: "Marron Marron", img: "/HaryanviImg/song2img.jpg", artist: "Kulwinder Billa", src: "/punjabi/songs2.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 3,song: "Defender", img: "/HaryanviImg/song3img.jpg",artist: "Harf Cheema", src: "/punjabi/songs3.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 4,song: "Young G.O.A.T", img: "/HaryanviImg/song4img.jpg",artist: "Gur Sidhu", src: "/punjabi/songs4.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    { Id: 5,song: "Lahore", img: "/HaryanviImg/song5img.jpg",artist: "Ok Hoye Paye Haan", src: "/punjabi/songs5.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    { Id: 5,song: "Lahore", img: "/HaryanviImg/song5img.jpg",artist: "Ok Hoye Paye ", src: "/punjabi/songs5.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     {  Id: 6,song: "LEHNGA", img: "/HaryanviImg/song6img.jpg",artist: "Diljit Dosanjh", src: "/punjabi/songs6.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     {  Id: 7,song: "Taskar ", img: "/HaryanviImg/song7img.jpg",artist: "Shree Brar", src: "/punjabi/songs7.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id:8,song: "QATAL ", img: "/HaryanviImg/song8img.jpg",artist: "Guru Randhawa ", src: "/punjabi/songs8.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    { Id: 9,song: "SNAPCHAT", img: "/HaryanviImg/song9img.jpg",artist: "Surjit Bhullar & Gurlez Akhtar ", src: "/punjabi/songs9.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    { Id: 9,song: "SNAPCHAT", img: "/HaryanviImg/song9img.jpg",artist: "Surjit Bhullar", src: "/punjabi/songs9.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     {  Id: 10,song: "Jutti Jharhke", img: "/HaryanviImg/song10img.jpg",artist: "Gurnam Bhullar", src: "/punjabi/songs10.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
 ]
 
@@ -23,7 +23,7 @@ export const bhojpuriSongs = [
     { Id: 5, song: "Excuses", img: "/bhojpuriImg/song5img.jpg",artist: "AP Dhillon", src: "/bhojpuri/songs5.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 6,song: "Insane", img: "/bhojpuriImg/song6img.jpg",artist: "AP Dhillon", src: "/bhojpuri/songs6.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 7,song: "Proper Patola", img: "/bhojpuriImg/song7img.jpg",artist: "Badshah", src: "/bhojpuri/songs7.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    {  Id:8,song: "Laung Da Lashkara", img: "/bhojpuriImg/song8img.jpg",artist: "Diljit Dosanjh", src: "/bhojpuri/songs8.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    {  Id:8,song: "Laung  Lashkara", img: "/bhojpuriImg/song8img.jpg",artist: "Diljit Dosanjh", src: "/bhojpuri/songs8.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 9,song: "High Rated Gabru", img: "/bhojpuriImg/song9img.jpg",artist: "Guru Randhawa", src: "/bhojpuri/songs9.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 10, song: "Patiala Peg", img: "/bhojpuriImg/song10img.jpg",artist: "Diljit Dosanjh", src: "/bhojpuri/songs10.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
 ]
@@ -32,11 +32,11 @@ export const haryanviSongs  = [
     { Id: 1,song: "Ramjhol Bole Gi ", img: "/PunjabiImg/song1img.jpg", artist: "Masoom Sharma", src: "/Haryanvi/songs1.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 2, song: "2 Khatole", img: "/PunjabiImg/song2img.jpg", artist: "Masoom Sharma", src: "/Haryanvi/songs2.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 3,song: "Uncha Nicha Rasta", img: "/PunjabiImg/song3img.jpg",artist: "Raj Mawar, Komal C ", src: "/Haryanvi/songs3.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    { Id: 4, song: "Hero Handa ", img: "/PunjabiImg/song4img.jpg",artist: "Khushi Baliyan Punit Choudhary Raj Mawer", src: "/Haryanvi/songs4.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    { Id: 5,song: "Tag Awara Ke", img: "/PunjabiImg/song5img.jpg",artist: "Amanraj Gill, Shivani Yadav, Shiva Choudhary", src: "/Haryanvi/songs5.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    { Id: 4, song: "Hero Handa ", img: "/PunjabiImg/song4img.jpg",artist: "Khushi Baliyan ", src: "/Haryanvi/songs4.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    { Id: 5,song: "Tag Awara Ke", img: "/PunjabiImg/song5img.jpg",artist: "Amanraj Gill", src: "/Haryanvi/songs5.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 6,song: "Sumit Parta ", img: "/PunjabiImg/song6img.jpg",artist: "Ghane Gande ", src: "/Haryanvi/songs6.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    { Id: 7,song: "Aankhya Te Maregi", img: "/PunjabiImg/song7img.jpg",artist: "Khushi Baliyan & Vivek Raghav ", src: "/Haryanvi/songs7.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
-    { Id: 8,song: "Dug Dug", img: "/PunjabiImg/song8img.jpg",artist: "Aman Jaji, Fiza Choudhary", src: "/Haryanvi/songs8.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    { Id: 7,song: "Aankhya Te Maregi", img: "/PunjabiImg/song7img.jpg",artist: "Khushi Baliyan ", src: "/Haryanvi/songs7.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
+    { Id: 8,song: "Dug Dug", img: "/PunjabiImg/song8img.jpg",artist: "Aman Jaji", src: "/Haryanvi/songs8.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 9,song: "Zehar", img: "/PunjabiImg/song9img.jpg",artist: "Sinta Bhai", src: "/Haryanvi/songs9.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
     { Id: 10,song: "60 MUKADME ", img: "/PunjabiImg/song10img.jpg",artist: "MASOOM SHARMA ", src: "/Haryanvi/songs10.mp3", playIcon: <FaPlay />, pauseIcon: <FaPause />, downloadIcon: <FaDownload /> },
 
