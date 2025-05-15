950cc65a377e8055f2c8296e2999c9e9bab1bc31
Aditya <ajha48450@gmail.com>
2025-05-15 11:40:18 +0530

    Some changes

--- Backend/ConnectDb.js
+++ Backend/ConnectDb.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose')
 
 //Mongo Connection Url
-const mongoURL='mongodb://127.0.0.1:27017/Music'
+// const mongoURL='mongodb://127.0.0.1:27017/Music'
+const mongoURL="mongodb+srv://adityakumar:adityakumar124786@cluster0.j6wdzx9.mongodb.net/"
 
 mongoose.connect(mongoURL,{
     useNewUrlParser: true,
