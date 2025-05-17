d26046dc6c5d543a98ae474aee92efd95d3ab6c6
Aditya <ajha48450@gmail.com>
2025-05-17 16:03:18 +0530

    upate route

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://music-application-backend.onrender.com/login', form, {
+      const res = await axios.post('https://music-application-backend.onrender.com/person/login', form, {
         
         headers: {
           'Content-Type': 'application/json',
