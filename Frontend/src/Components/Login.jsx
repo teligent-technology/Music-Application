d871f7916a83371d9e17079716e0a9060846600c
Aditya <ajha48450@gmail.com>
2025-05-22 21:40:30 +0530

    Some

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:3000/person/login', form, {
+      const res = await axios.post('https://music-application-backend.onrender.com/person/login', form, {
         headers: { 'Content-Type': 'application/json' },
       });
       if (res.status === 200) {
