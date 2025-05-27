be28899816c4d6470f4fe1f3db2921b8e33105d0
Aditya <ajha48450@gmail.com>
2025-05-27 15:18:46 +0530

    some

--- Frontend/src/Components/HomePage.jsx
+++ Frontend/src/Components/HomePage.jsx
@@ -100,6 +100,7 @@ const HomePage = () => {
   return (
     
     <div className="bg-dark text-white min-vh-100 pb-5">
+      {/* "bg-dark text-white min-vh-100 pb-5" */}
       {/* Header */}
       <Container fluid className="py-3 px-4 border-bottom border-secondary sticky-top bg-black z-3">
         <div className="d-flex flex-wrap justify-content-between align-items-center gap-3">
