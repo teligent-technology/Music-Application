15ee94de640f7fd21122607de7db1ec6bfbc5163
Aditya <ajha48450@gmail.com>
2025-06-08 23:15:30 +0530

    somee

--- Frontend/src/Pages/Punjabi.jsx
+++ Frontend/src/Pages/Punjabi.jsx
@@ -53,6 +53,38 @@ const Punjabi = () => {
       </nav>
 
       <AudioPlayer songsList={Songs} />
+
+
+      <div className="d-md-none position-fixed bottom-0 start-0 end-0 bg-dark text-white border-top border-secondary z-3">
+              <div className="d-flex justify-content-around py-2">
+                <Link to="/home" className="text-white text-center text-decoration-none">
+                  <i className="bi bi-house-door-fill fs-4 d-block" />
+                  <small>Home</small>
+                </Link>
+                <Link to="/search" className="text-white text-center text-decoration-none">
+                  <i className="bi bi-search fs-4 d-block" />
+                  <small>Search</small>
+                </Link>
+                <Link to="/punjabi" className="text-white text-center text-decoration-none">
+                  <i className="bi bi-music-note-list fs-4 d-block" />
+                  <small>Library</small>
+      
+                              {/* <Link to="/punjabi" className="btn btn-sm btn-dark rounded-pill px-4">Songs</Link> */}
+      
+                </Link>
+                <Link to="/create" className="text-white text-center text-decoration-none">
+                  <i className="bi bi-plus-circle-fill fs-4 d-block" />
+                  <small>Create</small>
+                </Link>
+                <Link to="/premium" className="text-white text-center text-decoration-none">
+                  <i className="bi bi-gem fs-4 d-block" />
+                  <small>Premium</small>
+                </Link>
+      
+      
+      
+              </div>
+            </div>
     </div>
   );
 };
