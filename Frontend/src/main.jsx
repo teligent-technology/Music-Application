355ce54deb2ae89b76fcedac04a682bf25529fa4
Aditya <ajha48450@gmail.com>
2025-06-15 20:25:47 +0530

    some changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import 'animate.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
